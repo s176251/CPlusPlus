5b9a82563a6c624f80c4e51c2708ac9bcf7a0bad
s176251 <s176251@users.noreply.github.com>
2014-02-05 09:37:24 +0100

    Create stringlong.cpp

---
+++ oblig1/stringlong.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <tchar.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+unsigned long long string_to_ulong(std::string s)
+{
+	const char* c = s.c_str();
+	unsigned long long* a = (unsigned long long*)c;
+	return *a;
+}
+
+std::string longlong_to_string(long long ll)
+{
+	const char* c = (char*)&ll;
+	string s(c,8);
+	return s;
+}
+
+int main(int argc, char* argv[])
+{
+
+	unsigned long long longstr = string_to_ulong("I love C++");  // String til long long
+	cout << longstr << endl;
+	
+	std::string strlong = longlong_to_string(longstr);           // long long til String
+	cout << strlong << endl;
+
+	int pause;
+	cin >> pause;  // Kun for å pause promptet.
+	return 0;
+}
