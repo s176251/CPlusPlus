765df817262a70a4bb3cac87c6d3171e7fdced8f
s176251 <s176251@stud.hioa.no>
2014-02-05 23:37:21 +0100

    stuff

--- oblig1/wordcloud.cpp
+++ oblig1/wordcloud.cpp
@@ -12,35 +12,6 @@ using namespace std;
 
 string legalizeString(string* strp)
 {
-	string str;
-	int a = strp->length();
-	bool skip = false;
-
-	for (int i = 0; i < a; i++)
-	{
-		char* ptr = (char*)stopchars_c;
-		while(*ptr)
-		{
-			if((*strp)[i] == *ptr)
-			{
-				skip = true;
-				break;
-			}
-			ptr++;
-		}
-
-		if(!skip)
-		{
-			str += tolower((*strp)[i]);
-		}
-		else
-			skip = false;
-	}
-	return str;
-}
-
-string legalizeString2(string* strp)
-{
 	int a = strp->length();
 
 	for (int i = strp->length(); i > 0; i--)
@@ -64,13 +35,11 @@ string legalizeString2(string* strp)
 
 int main(int argc, char* argv[])
 {
-	//vector <int> counts;
-	//vector <string> words;
 	std::map<std::string, int> wordCount;
 	std::map<std::string, int> ignore;
 	string line;
 	string word;
-	//getline(cin, a)
+
 	char* ptr = (char*)stopwords_c;
 	string str;
 
@@ -90,15 +59,15 @@ int main(int argc, char* argv[])
 
 	while(cin >> word)
 	{
-		word = legalizeString2(&word);
+		word = legalizeString(&word);
 		if(ignore.count(word) == 0)
 			wordCount[word]++;
 	}
-
+/*
 	for (map<string, int>::iterator it = wordCount.begin(); it != wordCount.end(); it++)
 	{
 		cout << it->first <<" : "<< it->second << endl;
 	}
-	
+*/
 	return 0;
 }
